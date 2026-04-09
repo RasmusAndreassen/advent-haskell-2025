0c5542b8ebd7c246d6dcb3b2c1cd87f3cc9bdf46
Rasmus Andreassen <r-andreassen@ankh-systems.co.jp>
2026-04-09 18:38:48 +0900

    Fikk ikkje mykje gjort i dag

--- day8/exe/Main.hs
+++ day8/exe/Main.hs
@@ -1,9 +1,19 @@
 module Main (main) where
 
+import Control.Arrow
+import Data.Function (on)
+import Data.Map (Map, (!))
+import Data.Map qualified as Map
+import GHC.Real (infinity)
 import Point
 
 getLines :: IO [String]
 getLines = lines <$> getContents
+
+nn d as =
+  let mp = Map.fromList $ (id &&& const infinity) <$> as
+      o = d `on` (mp !)
+   in mp
 
 main :: IO ()
 main = do
