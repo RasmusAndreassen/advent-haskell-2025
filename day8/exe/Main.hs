c24d54f12f66b0fae5538a34d410d5c383232d3d
Rasmus Andreassen <r-andreassen@ankh-systems.co.jp>
2026-04-10 18:31:14 +0900

    ikkje mykkje framgang

--- day8/exe/Main.hs
+++ day8/exe/Main.hs
@@ -1,19 +1,29 @@
 module Main (main) where
 
 import Control.Arrow
-import Data.Function (on)
+import Data.Function
 import Data.Map (Map, (!))
 import Data.Map qualified as Map
+import GHC.Arr (listArray)
 import GHC.Real (infinity)
 import Point
 
 getLines :: IO [String]
 getLines = lines <$> getContents
 
+toFst :: (a -> b) -> a -> (b, a)
+toFst f = f &&& id
+
+toSnd :: (a -> b) -> a -> (a, b)
+toSnd f = id &&& f
+
+nn :: (a -> a -> d) -> [a] -> Map a (a, Rational)
 nn d as =
-  let mp = Map.fromList $ (id &&& const infinity) <$> as
-      o = d `on` (mp !)
-   in mp
+  let arr = listArray $ toSnd (,infinity) <$> as
+   in foldl insert pl as
+  where
+    insert n_1 a =
+      n_1
 
 main :: IO ()
 main = do
