5d538fba9c4d3a08c8708d328af295884088c9ac
Rasmus Andreassen <rasmus.andreassen@proton.me>
2026-04-20 22:08:07 +0900

    Kom til fylling av område, treng bedre algoritme..

--- day9/exe/Data/Matrix/Lens.hs
+++ day9/exe/Data/Matrix/Lens.hs
@@ -1,23 +1,183 @@
-module Data.Matrix.Lens (sub) where
+module Data.Matrix.Lens
+  ( sub,
+    size,
+    asRows,
+    asCols,
+    elemAt,
+    eachPos,
+    eachElem,
+    eachRow,
+    eachCol,
+    row,
+    col,
+    rows,
+    cols,
+    removingRow,
+    removingCol,
+    removingRows,
+    removingCols,
+  )
+where
 
 import Control.Arrow
 import Control.Lens
 import Control.Lens qualified as Mat
-import Data.Matrix (Matrix (ncols, nrows), getElem, setElem, submatrix)
+import Control.Monad (guard)
+import Data.Foldable (Foldable (fold, foldr', toList))
+import Data.Matrix (Matrix (ncols, nrows), fromList, fromLists, getCol, getElem, getRow, mapCol, mapPos, mapRow, matrix, setElem, submatrix, toLists, transpose)
+import Data.Monoid (Endo (Endo))
+import Data.Vector qualified as Vec
+import Util
 
 elemAt :: (Int, Int) -> Lens' (Matrix a) a
 elemAt p@(i, j) = lens (getElem i j) (flip (`setElem` p))
+
+eachElem :: Traversal (Matrix a) (Matrix b) a b
+eachElem = traverse
+
+flattened :: Lens (Matrix a) (Matrix b) [a] [b]
+flattened f xa =
+  let (n, m) = xa ^. size
+   in fromList n m <$> (f . toList $ xa)
+
+eachPos :: IndexedTraversal (Int, Int) (Matrix a) (Matrix b) a b
+eachPos = itraverse . indexed
+
+eachRow :: Traversal (Matrix a) (Matrix b) [a] [b]
+eachRow = asRows . each
+
+eachCol :: Traversal (Matrix a) (Matrix b) [a] [b]
+eachCol = asCols . each
+
+asRows :: Iso (Matrix a) (Matrix b) [[a]] [[b]]
+asRows = iso toLists fromLists
+
+asCols :: Iso (Matrix a) (Matrix b) [[a]] [[b]]
+asCols = transposed . asRows
+
+transposed :: Iso (Matrix a) (Matrix b) (Matrix a) (Matrix b)
+transposed = iso transpose transpose
+
+setRow :: (Int -> a) -> Int -> Matrix a -> Matrix a
+setRow as = mapRow (const . as)
+
+setCol :: (Int -> a) -> Int -> Matrix a -> Matrix a
+setCol as = mapCol (const . as)
+
+setRowTo :: [a] -> Int -> Matrix a -> Matrix a
+setRowTo as = setRow (\n -> as !! (n - 1))
+
+setColTo :: [a] -> Int -> Matrix a -> Matrix a
+setColTo as = setCol (\n -> as !! (n - 1))
+
+row :: Int -> Lens' (Matrix a) [a]
+row i = lens (toList . getRow i) (writeRow i)
+  where
+    writeRow i m r = setRowTo r i m
+
+rows :: [Int] -> Lens' (Matrix a) (Matrix a)
+rows is = lens (getter is) (setter is)
+  where
+    getter is x =
+      let rs = (toList .: getRow) <$> is ?? x
+       in fromLists rs
+    setter is x rm =
+      let rs = rm ^.. eachRow
+          settings = zipWith setRowTo rs is
+       in ala Endo foldMap settings x
+
+col :: Int -> Lens' (Matrix a) [a]
+col j = lens (toList . getCol j) (writeCol j)
+  where
+    writeCol j m c = setColTo c j m
+
+cols :: [Int] -> Lens' (Matrix a) (Matrix a)
+cols js = lens (getter js) (setter js)
+  where
+    getter js x =
+      let cs = (toList .: getCol) <$> js ?? x
+       in transpose $ fromLists cs
+    setter js x cm =
+      let cs = cm ^.. eachCol
+          settings = zipWith setColTo cs js
+       in ala Endo foldMap settings x
 
 size :: Getter (Matrix a) (Int, Int)
 size = to (nrows &&& ncols)
 
 writeSubMatrix :: Int -> Int -> Matrix a -> Matrix a -> Matrix a
-writeSubMatrix i0 j0 as m =
+writeSubMatrix i0 j0 m as =
   let (imax, jmax) = as ^. size
-      ijs = [(i, j) | i <- [1 .. imax], j <- [1 .. jmax]]
-      adj = ((+ i0) *** (+ j0))
-      transfer m p = m & elemAt (adj p) .~ (as ^. elemAt p)
-   in foldl transfer m ijs
+      adj = ((+ (i0 - 1)) *** (+ (j0 - 1)))
+      transfer p m a = m & elemAt (adj p) .~ a
+   in ifoldl transfer m as
+
+removeRow :: Int -> Matrix a -> Matrix a
+removeRow r x =
+  let (n, m) = x ^. size
+      n' = n - 1
+      adjusted p@(i, j)
+        | i < r = p
+        | otherwise = first (+ 1) p
+      procure p = x ^. elemAt (adjusted p)
+   in matrix n' m procure
+
+removingRow :: Int -> Lens' (Matrix a) (Matrix a)
+removingRow j = lens (removeRow j) (writeAroundRow j)
+  where
+    writeAroundRow r x s =
+      let procure p@(i, j)
+            | i < r = const $ s ^. elemAt p
+            | i > r = const $ s ^. elemAt (i - 1, j)
+            | otherwise = id
+       in imap procure x
+
+removeCol :: Int -> Matrix a -> Matrix a
+removeCol c x =
+  let (n, m) = x ^. size
+      m' = m - 1
+      adjusted p@(i, j)
+        | j < c = p
+        | otherwise = second (+ 1) p
+      procure p = x ^. elemAt (adjusted p)
+   in matrix n m' procure
+
+removingCol :: Int -> Lens' (Matrix a) (Matrix a)
+removingCol c = lens (removeCol c) (writeAroundCol c)
+  where
+    writeAroundCol c x s =
+      let procure p@(i, j)
+            | j < c = const $ s ^. elemAt p
+            | j > c = const $ s ^. elemAt (i, j - 1)
+            | otherwise = id
+       in imap procure x
+
+removingCols :: [Int] -> Lens' (Matrix a) (Matrix a)
+removingCols (j : js) =
+  let js' = js & traverse . filtered (> j) -~ 1
+   in removingCol j . removingCols js'
+removingCols [] = id
+
+removingRows :: [Int] -> Lens' (Matrix a) (Matrix a)
+removingRows (i : is) =
+  let is' = is & traverse . filtered (> i) -~ 1
+   in removingRow i . removingRows is'
+removingRows [] = id
+
+droppingCols :: Int -> Lens' (Matrix a) (Matrix a)
+droppingCols m = removingCols [1 .. m]
+
+droppingRows :: Int -> Lens' (Matrix a) (Matrix a)
+droppingRows n = removingRows [1 .. n]
+
+instance FunctorWithIndex (Int, Int) Matrix
+
+instance FoldableWithIndex (Int, Int) Matrix
+
+instance TraversableWithIndex (Int, Int) Matrix where
+  itraverse f x =
+    let x' = mapPos f x
+     in sequenceA x'
 
 sub :: (Int, Int) -> (Int, Int) -> Lens' (Matrix a) (Matrix a)
 sub (i, j) (i', j') = lens (submatrix i i' j j') (writeSubMatrix i j)
--- day9/exe/Main.hs
+++ day9/exe/Main.hs
@@ -2,14 +2,13 @@ module Main (main) where
 
 import Control.Arrow
 import Control.Lens
-import Control.Monad (guard)
-import Control.Monad.State (State, gets)
+import Control.Monad (guard, when)
+import Control.Monad.State (MonadState (get, put), State, evalState, gets)
 import Data.Bifoldable (biminimum)
 import Data.Matrix
-import Data.Matrix.Lens (sub)
+import Data.Matrix.Lens
 import Data.Maybe (fromMaybe)
 import Data.Semigroup (Max (Max, getMax), Min (Min, getMin))
-import GHC.Generics
 import Util
 import Vector
 
@@ -19,16 +18,16 @@ diff = ((+ 1) . abs) .: (-)
 area :: Vector -> Vector -> Int
 area (V x1 y1) (V x2 y2) = x1 `diff` x2 * y1 `diff` y2
 
-extremaOf :: Getting (Min a, Max a) s a -> s -> (a, a)
-extremaOf f =
+extrema :: Getting (Min a, Max a) s a -> s -> (a, a)
+extrema f =
   let wrap = Const . (Min &&& Max)
       unwrap = (getMin *** getMax) . getConst
    in unwrap . f wrap
 
 between :: (Vector, Vector) -> Vector -> Bool
 between b (V vx vy) =
-  let (minx, maxx) = extremaOf (traverse . x) b
-      (miny, maxy) = extremaOf (traverse . y) b
+  let (minx, maxx) = extrema (traverse . x) b
+      (miny, maxy) = extrema (traverse . y) b
    in and
         [ minx < vx,
           vx < maxx,
@@ -43,24 +42,61 @@ squares vs = do
   guard $ none (between (v1, v2)) vs
   return $ area v1 v2
 
-data Tile = White | Red | Green deriving (Show, Eq, Enum)
+data Tile = W | R | G deriving (Show, Eq, Enum)
 
-colorFloor :: Vector -> Matrix Tile -> State (Maybe Vector) (Matrix Tile)
-colorFloor p@(V x1 y1) floor = do
-  (V x0 y0) <- gets $ fromMaybe p
-  let (minX, maxX) = extremaOf traverse [x0, x1]
-      (minY, maxY) = extremaOf traverse [y0, y1]
-  return $ floor & sub (minX, minY) (maxX, maxY) . traverse .~ Green
+colorFloor :: Vector -> State (Maybe Vector, Matrix Tile) ()
+colorFloor p1@(V x1 y1) = do
+  p0@(V x0 y0) <- use $ _1 . to (fromMaybe p1)
+  floor <- use _2
+  let (minX, maxX) = extrema traverse [x0, x1]
+      (minY, maxY) = extrema traverse [y0, y1]
+
+  _1 .= Just p1
+  _2
+    .= ( floor
+           & sub (minX, minY) (maxX, maxY) . eachElem .~ G
+           & elemAt (x0, y0) .~ R
+           & elemAt (x1, y1) .~ R
+       )
+
+brush :: Tile -> State Tile Tile
+brush t = do
+  b <- get
+  case b of
+    W -> do
+      put t
+      return t
+    G -> do
+      when (t == G) $
+        put W
+      when (t == R) $
+        put R
+      return $
+        if t == R then R else G
+    R -> do
+      when (t /= G) $
+        put W
+      return t
+
+fillArea :: Matrix Tile -> Matrix Tile
+fillArea =
+  eachRow %~ \row ->
+    let row' = mapM brush row
+        r = evalState row' W
+     in r
 
 main :: IO ()
 main = do
   tiles :: [Vector] <- map read . lines <$> getContents
-  let (baseX, _boundX) = extremaOf (traverse . x) tiles
-      (baseY, _boundY) = extremaOf (traverse . y) tiles
-      base = -V baseX baseY
-      boundX = _boundX - baseX
-      boundY = _boundY - baseY
-      adjustedTiles = tiles <&> (+ base)
-      floor = matrix boundX boundY (const White)
+  let (baseX, _boundX) = extrema (traverse . x) tiles
+      (baseY, _boundY) = extrema (traverse . y) tiles
+      adjustment = V baseX baseY
+      boundX = _boundX - baseX + 1
+      boundY = _boundY - baseY + 1
+      adjustedTiles = tiles |> head tiles <&> (+ (-adjustment + 1))
+      floor = matrix boundX boundY (const W)
+      coloring = mapM_ colorFloor adjustedTiles
+      floor' = ((Nothing, floor) &~ coloring) ^. _2
+      floor'' = fillArea floor'
 
-  print floor
+  print floor''
